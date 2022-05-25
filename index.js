a19bb8ed050d70344d9467f5bb44ee66e213fea4
Amethyst Bibby <amethystbibby@gmail.com>
2022-05-25 18:46:29 +1000

    Tidied up code

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules/flowbite'));
 app.set('view engine', 'ejs');
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/public/dist/index.html');
-// })
-
 // TEST MONGO CONNECTION
 const uri =
 `mongodb+srv://${username}:${password}@laddi.1dxhy.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,35 +24,14 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
     console.log('Connected to Database')
     const db = client.db('main')
     const courses = db.collection('courses');
+    console.log(db.collection("firstCollection").find().toArray())
 
     app.get('/', (req, res) => {
         db.collection("courses").find().toArray()
           .then(data => {
               res.render('index.ejs', {courses : data})
+              console.log(data);
           })
           .catch(error => console.error(error));
       });
    });
-
-// const client = new MongoClient(uri);
-// async function run(courses) {
-//     try {
-//         await client.connect();
-//         const database = client.db('main');
-//         // const courses = await database.collection('courses').find().toArray();
-
-//         app.get('/', (req, res) => {
-//             database.collection("courses").find().toArray()
-//               .then(data => {
-//                   res.render('index.ejs', {courses : data})
-//               })
-//               .catch(/* ... */);
-//           })
-//         return courses;
-//     } finally {
-//         // Ensures that the client will close when you finish/error
-//         await client.close();
-//     }
-// }
-
-// run().catch(console.dir);
